2c4be897ac0283f66e16c09a88e9b68ec4f72d8e
chetverik2 <chetverik.prog@gmail.com>
2025-01-13 13:59:18 +0300

    first commit

---
+++ 0.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers) {
+    if (!numbers.lenght === 10) {
+        console.log("не все цифры")
+    } else {
+
+    }
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+
+const scrollArrowClass = document.getElementsByClassName('header-arrow')[0];
+
+scrollArrowClass.addEventListener('click', function (element) {
+   console.log(element)
+    window.scrollBy({
+        top: 700, 
+        left: 0,   
+        behavior: 'smooth' 
+    });
+});
